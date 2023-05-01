435c9c689cafeede4282d6d3d84a22752cb25683
tbohn2 <tanner.bohn@yahoo.com>
2023-04-30 21:37:57 -0700

    Added more prompts; created Square class

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const { choices } = require('yargs');
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'Please enter 3 characters for logo: '
+const selection = () =>
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Please enter 3 characters for logo: '
+            },
+            {
+                type: 'input',
+                name: 'txtColor',
+                message: 'What color would you like the text to be?'
+            },
+            {
+                type: 'input',
+                name: 'bgColor',
+                message: 'What color would you like the background to be?'
+            },
+            {
+                type: 'list',
+                name: 'shape',
+                message: 'What shape do you want for your logo?',
+                choices: ['square', 'circle', 'triangle']
+            }
+        ])
+        .then((sel) => {
+            if (sel.shape === 'square') {
+
+            }
+            return (title);
         }
-    ])
-    .then(({ title }) => {
-        console.log(title);
-    }
-    )
+        )
 
-// const text = new Text();
+selection();
 
